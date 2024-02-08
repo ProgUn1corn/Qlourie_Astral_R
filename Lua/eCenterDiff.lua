97b9eeab7da113a582516debce48e8bda2f31b20
Prog Un1corn <50629932+ProgUn1corn@users.noreply.github.com>
2024-02-08 21:39:56 +0800

    Fix errors

--- Lua/eCenterDiff.lua
+++ Lua/eCenterDiff.lua
@@ -18,44 +18,47 @@ local oldLockCoef = 0
 local rearBias = 0
 
 local function updateWheelsIntermediate()
-  if transfercase then --get input value
-    local throttle = electrics.values['throttle_input']
-    local brake = electrics.values['brake_input']
-    local steer = electrics.values['steering_input']
-    
-    --get lock range
-    maxLockCoef = transfercase.lsdLockCoef
-    minLockCoef = transfercase.lsdRevLockCoef
-    if minLockCoef - maxLockCoef < 0 then --make sure min is larger than max
-      local lockRange = 1
-    else
-      local lockRange = abs(maxLockCoef - minLockCoef) 
-    end
-    
-    --calculate new lock coef and bias
-    local normalSteer = abs(steer) --make value 0 to 1
-    local normalThrottle = abs(throttle-brake) --make value 0 to 1
-    local contributionThrottle = lockRange*normalThrottle + minLockCoef
-    local contributionSteer = lockRange*normalSteer + minLockCoef
-    local lockEffect = max(0,min(contributionThrottle-contributionSteer,lockRange))
-    newLockCoef = minLockCoef + lockEffect
-    rearBias = 0.5 + normalSteer*0.2
+  
+  --get input value
+  local throttle = electrics.values['throttle_input']
+  local brake = electrics.values['brake_input']
+  local steer = electrics.values['steering_input']
+  local lockRange = 0
 
-    --apply values to diff
-    transfercase.lsdLockCoef = newLockCoef
-    transfercase.lsdRevLockCoef = minLockCoef
-    transfercase.diffTorqueSplitA = rearBias
-    transfercase.diffTorqueSplitB = 1 - rearBias
+  --get lock range
+  maxLockCoef = transfercase.lsdLockCoef
+  minLockCoef = transfercase.lsdRevLockCoef
+  if minLockCoef - maxLockCoef < 0 then --make sure min is larger than max
+    lockRange = 1
+  else
+    lockRange = abs(maxLockCoef - minLockCoef) 
   end
+
+  --calculate new lock coef and bias
+  local normalSteer = abs(steer) --make value 0 to 1
+  local normalThrottle = abs(throttle-brake) --make value 0 to 1
+  local contributionThrottle = lockRange*normalThrottle + minLockCoef
+  local contributionSteer = lockRange*normalSteer + minLockCoef
+  local lockEffect = max(0,min(contributionThrottle-contributionSteer,lockRange))
+  newLockCoef = minLockCoef + lockEffect
+  rearBias = 0.756 + normalSteer*0.244
+
+  --apply values to diff
+  transfercase.lsdLockCoef = newLockCoef
+  transfercase.lsdRevLockCoef = minLockCoef
+  transfercase.diffTorqueSplitA = 1 - rearBias
+  transfercase.diffTorqueSplitB = rearBias
+
+  --handbrake
+  if input.parkingbrake > 0.5 then
+    transfercase.lsdLockCoef = 0
+  end  
 end
 
 local function init(jbeamData)
   transfercase = powertrain.getDevice(jbeamData.transfercaseName)
-  if transfercase then
-    oldLockCoef = transfercase.lsdLockCoef
-    M.updateWheelsIntermediate = updateWheelsIntermediate
-    M.updateGFX = updateGFX
-  end
+  M.updateWheelsIntermediate = updateWheelsIntermediate
+  M.updateGFX = updateGFX
 end
 
 M.init = init
