c56c00d2a261a766b068f278920957920a4c910c
Prog Un1corn <50629932+ProgUn1corn@users.noreply.github.com>
2024-02-18 00:49:13 +0800

    F and R both working

--- Lua/LRS.lua
+++ Lua/LRS.lua
@@ -9,20 +9,29 @@ local min = math.min
 local max = math.max
 
 --local gMass = {}
-local dampers = {}
-local damping = {}
-local loads = {}
-local activeFlag = 0
+local fDamping = {}
+local rDamping = {}
+local fLoads = {}
+local rLoads = {}
+local fDampers = {}
+local rDampers = {}
 
-
-function clamp(value, min, max)
-  return math.min(math.max(value, min), max)
+function printTable(t, indent)
+  indent = indent or ""
+  for k, v in pairs(t) do
+    if type(v) == "table" then
+      print(indent .. "[" .. k .. "] => Table:")
+      printTable(v, indent .. "  ")
+    else
+      print(indent .. "[" .. k .. "] => " .. tostring(v))
+    end
+  end
 end
 
 local function updateWheelsIntermediate()
-  for i, spring in ipairs(loads) do
-    --get suspension load
-    local springLoad = clamp(0-obj:getBeamStress(spring.bCid),0,9999) or 0
+  --front and rear separate process
+  for i, spring in ipairs(fLoads) do
+    local springLoad = clamp(0-obj:getBeamStress(spring.bCid),0,9999) or 0 --get suspension load
     if springLoad <= 1500 then 
       spring.activeFlag = true
     else
@@ -30,23 +39,49 @@ local function updateWheelsIntermediate()
     end
   end  
 
-  for i, damper in ipairs(dampers) do
-    if loads[i].activeFlag == true then
-      damper.newLSRebound = damping[2].beamDampRebound
-      damper.newHSRebound = damping[2].beamDampReboundFast
+  for i, damper in ipairs(fDampers) do
+    if fLoads[i].activeFlag == true then --active LRS
+      damper.newLSRebound = fDamping[2].beamDampRebound
+      damper.newHSRebound = fDamping[2].beamDampReboundFast
     else
       damper.newLSRebound = damper.orgLSRebound
       damper.newHSRebound = damper.orgHSRebound
     end
-    --print(loads[1].activeFlag)
-
+    --print(fLoads[1].activeFlag)
+    --print(fLoads[2].activeFlag)
+    
     --apply new rebound
-    obj:setBoundedBeamDamp(damper.bCid, damping[1].beamDamp, damper.newLSRebound, damping[1].beamDampFast, damper.newHSRebound, damping[1].beamDampVelocitySplit, damping[1].beamDampVelocitySplitRebound) 
+    obj:setBoundedBeamDamp(damper.bCid, fDamping[1].beamDamp, damper.newLSRebound, fDamping[1].beamDampFast, damper.newHSRebound, fDamping[1].beamDampVelocitySplit, fDamping[1].beamDampVelocitySplitRebound) 
+    --print(fDampers[1].newHSRebound)
+    --print(fDampers[2].newHSRebound)
   end
-  --print(dampers[1].newHSRebound)
-  --print(dampers[2].newHSRebound)
-end
+  
+  for i, spring in ipairs(rLoads) do
+    local springLoad = clamp(0-obj:getBeamStress(spring.bCid),0,9999) or 0 --get suspension load
+    if springLoad <= 1500 then 
+      spring.activeFlag = true
+    else
+      spring.activeFlag = false
+    end
+  end  
 
+  for i, damper in ipairs(rDampers) do
+    if rLoads[i].activeFlag == true then --active LRS
+      damper.newLSRebound = rDamping[2].beamDampRebound
+      damper.newHSRebound = rDamping[2].beamDampReboundFast
+    else
+      damper.newLSRebound = damper.orgLSRebound
+      damper.newHSRebound = damper.orgHSRebound
+    end
+    --print(rLoads[1].activeFlag)
+    --print(rLoads[2].activeFlag)
+    
+    --apply new rebound
+    obj:setBoundedBeamDamp(damper.bCid, rDamping[1].beamDamp, damper.newLSRebound, rDamping[1].beamDampFast, damper.newHSRebound, rDamping[1].beamDampVelocitySplit, rDamping[1].beamDampVelocitySplitRebound) 
+    --print(rDampers[1].newHSRebound)
+    --print(rDampers[2].newHSRebound)
+  end
+end
 
 local function init(jbeamData)
   local beamNameTable = {}
@@ -55,40 +90,75 @@ local function init(jbeamData)
       beamNameTable[b.name] = b.cid
     end
   end
+  
+  --call out damping values in Jbeam
+  fDamping = tableFromHeaderTable(jbeamData.fDamping or {}) 
+  rDamping = tableFromHeaderTable(jbeamData.rDamping or {})
+  
+  fLoads = {}
+  rLoads = {}
+  fDampers = {}
+  rDampers = {}
 
-  loads = {}
-  dampers = {}
-  damping = {}
-
-  local spring = tableFromHeaderTable(jbeamData.loads or {}) --call out spring load in Jbeam
-  for _, load in pairs(spring) do
-    local bCid = beamNameTable[load.beamName]
-    local l = {
-      name = load.name,
+  --front and rear separate process
+  local fSpring = tableFromHeaderTable(jbeamData.fLoads or {}) --call out spring load in Jbeam
+  for _, loads in pairs(fSpring) do
+    local bCid = beamNameTable[loads.beamName]
+    local fl = {
+      name = loads.name,
       bCid = bCid,
       activeFlag = false
     }
-    table.insert(loads, l)
+    table.insert(fLoads, fl)
+  end
+
+  local rSpring = tableFromHeaderTable(jbeamData.rLoads or {})
+  for _, loads in pairs(rSpring) do
+    local bCid = beamNameTable[loads.beamName]
+    local rl = {
+      name = loads.name,
+      bCid = bCid,
+      activeFlag = false
+    }
+    table.insert(rLoads, rl)
   end
   
-  local LRS = tableFromHeaderTable(jbeamData.dampers or {}) --call out damper in Jbeam
-  damping = tableFromHeaderTable(jbeamData.damping or {}) --call out damping values in Jbeam
-  for _, damper in pairs(LRS) do
+  local fLRS = tableFromHeaderTable(jbeamData.fDampers or {}) --call out damper in Jbeam
+  for _, damper in pairs(fLRS) do
     local bCid = beamNameTable[damper.beamName]
-    local d = {
+    local fd = {
       name = damper.name,
       bCid = bCid,
-      orgLSRebound = damping[1].beamDampRebound,
-      orgHSRebound = damping[1].beamDampReboundFast,
-      newLSRebound = damping[2].beamDampRebound,
-      newHSRebound = damping[2].beamDampReboundFast,
+      orgLSRebound = fDamping[1].beamDampRebound,
+      orgHSRebound = fDamping[1].beamDampReboundFast,
+      newLSRebound = fDamping[2].beamDampRebound,
+      newHSRebound = fDamping[2].beamDampReboundFast,
     }
-    table.insert(dampers, d)
+    table.insert(fDampers, fd)
+  end
+
+  local rLRS = tableFromHeaderTable(jbeamData.rDampers or {})
+  for _, damper in pairs(rLRS) do
+    local bCid = beamNameTable[damper.beamName]
+    local rd = {
+      name = damper.name,
+      bCid = bCid,
+      orgLSRebound = rDamping[1].beamDampRebound,
+      orgHSRebound = rDamping[1].beamDampReboundFast,
+      newLSRebound = rDamping[2].beamDampRebound,
+      newHSRebound = rDamping[2].beamDampReboundFast,
+    }
+    table.insert(rDampers, rd)
   end
   
-  --print(orgLSRebound)
-  --print(loads[1].bCid)
-  --print(loads[2].bCid)
+  --print(fDampers[1].bCid)
+  --printTable(fDampers)
+  --printTable(fLoads)
+  --printTable(fDamping)
+  
+  --printTable(rDampers)
+  --printTable(rLoads)
+  --printTable(rDamping)
 
   M.updateWheelsIntermediate = updateWheelsIntermediate
   M.updateGFX = updateGFX
