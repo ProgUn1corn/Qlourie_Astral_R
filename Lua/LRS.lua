0a03cdcf5104c841266a862113e2bd87fdb6bb01
Prog Un1corn <50629932+ProgUn1corn@users.noreply.github.com>
2024-02-17 20:39:07 +0800

    Update LRS.lua

--- Lua/LRS.lua
+++ Lua/LRS.lua
@@ -8,8 +8,11 @@ local abs = math.abs
 local min = math.min
 local max = math.max
 
-local gMass = {}
+--local gMass = {}
 local dampers = {}
+local damping = {}
+local loads = {}
+local activeFlag = 0
 local orgLSRebound = 0
 local orgHSRebound = 0
 local newLSRebound = 0
@@ -21,19 +24,30 @@ function clamp(value, min, max)
 end
 
 local function updateWheelsIntermediate()
-  local restMassLengthR = obj:getBeamLength(gMass[1].bCid)
-  local restMassLengthL = obj:getBeamLength(gMass[2].bCid)
-
-  if restMassLengthR > 0.25 then
-    obj:setBoundedBeamDamp(dampers[1].bCid, 1500, 1600, 5000, 1600, 0.65, 0.55)
+  for _, spring in ipairs(loads) do
+    --get suspension load
+    local springLoad = clamp(0-obj:getBeamStress(spring.bCid),0,9999) or 0
+    activeFlag = false
+    if springLoad <= 1500 then 
+      activeFlag = true
+      newLSRebound = damping[2].beamDampRebound
+      newHSRebound = damping[2].beamDampReboundFast
+    else
+      activeFlag = false
+      newLSRebound = orgLSRebound
+      newHSRebound = orgHSRebound
+    end
+    --print(activeFlag)
   end
-
-  if restMassLengthL > 0.25 then
-    obj:setBoundedBeamDamp(dampers[2].bCid, 1500, 1600, 5000, 1600, 0.65, 0.55)
+  
+  for _, damper in ipairs(dampers) do
+    --apply new rebound
+    if activeFlag == true then
+      obj:setBoundedBeamDamp(damper.bCid, damping[1].beamDamp, newLSRebound, damping[1].beamDampFast, newHSRebound, damping[1].beamDampVelocitySplit, damping[1].beamDampVelocitySplitRebound)
+    else
+      obj:setBoundedBeamDamp(damper.bCid, damping[1].beamDamp, orgLSRebound, damping[1].beamDampFast, orgHSRebound, damping[1].beamDampVelocitySplit, damping[1].beamDampVelocitySplitRebound)
+    end
   end
-  --print(restMassLength)
-  --for _, v in ipairs(damper) do
-  --end
 end
 
 local function init(jbeamData)
@@ -44,34 +58,37 @@ local function init(jbeamData)
     end
   end
 
+  loads = {}
   dampers = {}
-  local shock = tableFromHeaderTable(jbeamData.dampers or {}) --call out damper in Jbeam
-  for _, damperData in pairs(shock) do --store LRS
-    local bCid = beamNameTable[damperData.beamName]
+  damping = {}
+
+  local spring = tableFromHeaderTable(jbeamData.loads or {}) --call out spring load in Jbeam
+  for _, load in pairs(spring) do
+    local bCid = beamNameTable[load.beamName]
+    local l = {
+      name = load.name,
+      bCid = bCid,
+    }
+    table.insert(loads, l)
+  end
+  
+  local LRS = tableFromHeaderTable(jbeamData.dampers or {}) --call out damper in Jbeam
+  for _, damper in pairs(LRS) do
+    local bCid = beamNameTable[damper.beamName]
     local d = {
-      name = damperData.name,
+      name = damper.name,
       bCid = bCid,
     }
     table.insert(dampers, d)
   end
   
-  
-  gMass = {}
-  local LRS = tableFromHeaderTable(jbeamData.gmass or {}) --call out LRS in Jbeam
-  for _, damperData in pairs(LRS) do --store LRS
-    local bCid = beamNameTable[damperData.beamName]
-    local l = {
-      name = damperData.name,
-      bCid = bCid,
-      initialBeamLength = obj:getBeamRestLength(beamNameTable[damperData.beamName]),
-    }
-    table.insert(gMass, l)
-  end
+  damping = tableFromHeaderTable(jbeamData.damping or {}) --call out damping values in Jbeam
+  orgLSRebound = damping[1].beamDampRebound
+  orgHSRebound = damping[1].beamDampReboundFast
 
-  --print(dampers[1].name)
-  --print(dampers[2].name)
-  --print(gMass[1].name)
-  --print(gMass[1].name)
+  --print(orgLSRebound)
+  --print(loads[1].bCid)
+  --print(loads[2].bCid)
 
   M.updateWheelsIntermediate = updateWheelsIntermediate
   M.updateGFX = updateGFX
