0123293c24287c540efb5e74fa2ba08ae7a8cc2a
Prog Un1corn <50629932+ProgUn1corn@users.noreply.github.com>
2024-03-15 11:26:28 +0800

    LRS closed for now

--- Lua/LRS.lua
+++ Lua/LRS.lua
@@ -36,57 +36,55 @@ local function updateWheelsIntermediate()
   --front and rear separate process
   for i, spring in ipairs(fLoads) do
     local springLoad = obj:getBeamStress(spring.bCid) or 0 --get suspension load
-    if springLoad >= 0.648 then 
+    if springLoad >= -465 then 
       spring.activeFlag = true
     else
       spring.activeFlag = false
     end
     --print(obj:getBeamStress(fLoads[1].bCid))
+    for i, damperF in ipairs(fDampers) do
+      if fLoads[i].activeFlag == true then --active LRS
+        damperF.newLSRebound = clamp(fDamping[1].beamDampRebound-(fDamping[1].beamDampRebound-fDamping[2].beamDampRebound)*(1-springLoad/-465), fDamping[2].beamDampRebound, fDamping[1].beamDampRebound)
+        damperF.newHSRebound = damperF.newLSRebound
+      else
+        damperF.newLSRebound = damperF.orgLSRebound
+        damperF.newHSRebound = damperF.orgHSRebound
+      end
+      --print(fLoads[1].activeFlag)
+      --print(fLoads[2].activeFlag)
+      
+      --apply new rebound
+      obj:setBoundedBeamDamp(damperF.bCid, fDamping[1].beamDamp, damperF.newLSRebound, fDamping[1].beamDampFast, damperF.newHSRebound, fDamping[1].beamDampVelocitySplit, fDamping[1].beamDampVelocitySplitRebound) 
+      print(fDampers[1].newLSRebound)
+      --print(fDampers[2].newLSRebound)
+    end
   end  
 
-  for i, damperF in ipairs(fDampers) do
-    if fLoads[i].activeFlag == true then --active LRS
-      damperF.newLSRebound = clamp(fDamping[2].beamDampRebound, 1000, 2500)
-      damperF.newHSRebound = clamp(fDamping[2].beamDampReboundFast, 1000, 2500)
-    else
-      damperF.newLSRebound = damperF.orgLSRebound
-      damperF.newHSRebound = damperF.orgHSRebound
-    end
-    --print(fLoads[1].activeFlag)
-    --print(fLoads[2].activeFlag)
-    
-    --apply new rebound
-    obj:setBoundedBeamDamp(damperF.bCid, fDamping[1].beamDamp, damperF.newLSRebound, fDamping[1].beamDampFast, damperF.newHSRebound, fDamping[1].beamDampVelocitySplit, fDamping[1].beamDampVelocitySplitRebound) 
-    --print(fDampers[1].newLSRebound)
-    --print(fDampers[2].newLSRebound)
-  end
-  
   for i, spring in ipairs(rLoads) do
     local springLoad = obj:getBeamStress(spring.bCid) or 0 --get suspension load
-    if springLoad >= 0.648 then 
+    if springLoad >= -525 then 
       spring.activeFlag = true
     else
       spring.activeFlag = false
     end
     --print(obj:getBeamStress(rLoads[1].bCid))
-  end  
-
-  for i, damperR in ipairs(rDampers) do
-    if rLoads[i].activeFlag == true then --active LRS
-      damperR.newLSRebound = clamp(rDamping[2].beamDampRebound, 1000, 2500)
-      damperR.newHSRebound = clamp(rDamping[2].beamDampReboundFast, 1000, 2500)
-    else
-      damperR.newLSRebound = damperR.orgLSRebound
-      damperR.newHSRebound = damperR.orgHSRebound
+    for i, damperR in ipairs(rDampers) do
+      if rLoads[i].activeFlag == true then --active LRS
+        damperR.newLSRebound = clamp(rDamping[1].beamDampRebound-(rDamping[1].beamDampRebound-rDamping[2].beamDampRebound)*(1-springLoad/-525), rDamping[2].beamDampRebound, rDamping[1].beamDampRebound)
+        damperR.newHSRebound = damperR.newLSRebound
+      else
+        damperR.newLSRebound = damperR.orgLSRebound
+        damperR.newHSRebound = damperR.orgHSRebound
+      end
+      --print(rLoads[1].activeFlag)
+      --print(rLoads[2].activeFlag)
+      
+      --apply new rebound
+      obj:setBoundedBeamDamp(damperR.bCid, rDamping[1].beamDamp, damperR.newLSRebound, rDamping[1].beamDampFast, damperR.newHSRebound, rDamping[1].beamDampVelocitySplit, rDamping[1].beamDampVelocitySplitRebound) 
+      --print(rDampers[1].newHSRebound)
+      --print(rDampers[2].newHSRebound)
     end
-    --print(rLoads[1].activeFlag)
-    --print(rLoads[2].activeFlag)
-    
-    --apply new rebound
-    obj:setBoundedBeamDamp(damperR.bCid, rDamping[1].beamDamp, damperR.newLSRebound, rDamping[1].beamDampFast, damperR.newHSRebound, rDamping[1].beamDampVelocitySplit, rDamping[1].beamDampVelocitySplitRebound) 
-    --print(rDampers[1].newHSRebound)
-    --print(rDampers[2].newHSRebound)
-  end
+  end  
 end
 
 local function init(jbeamData)
