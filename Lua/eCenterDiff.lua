40a561de006899c17b4eb16464e976eede212c2b
Prog Un1corn <50629932+ProgUn1corn@users.noreply.github.com>
2024-04-02 01:11:50 +0800

    diff update

--- Lua/eCenterDiff.lua
+++ Lua/eCenterDiff.lua
@@ -44,110 +44,124 @@ function printTable(t, indent)
 end
 
 local function updateWheelsIntermediate()
-  --get input value
-  throttle = electrics.values['throttle_input'] or 0
-  brake = electrics.values['brake_input'] or 0
-  steer = electrics.values['steering_input'] or 0
-  speed = electrics.values.wheelspeed*3.6 or 0 --m/s to km/h
-  local normalSteer = abs(steer) --make value 0 to 1
-  local clampSpeed = clamp(speed, 0, 120) --make speed-steer factor only affect 0-120 km/h
-  
-  --steering contribution with countersteer control
-  local yaw = obj:getYawAngularVelocity() --left is positive
-  if steer*yaw < 0 then
-    xLockCoef = 0
-  else
-    xLockCoef = clamp(lockRange*normalSteer*steerRatio, 0, 1) --calculate steering contribution
-  end
+  if transferType == "LSD" then 
+    --get input value
+    throttle = electrics.values['throttle_input'] or 0
+    brake = electrics.values['brake_input'] or 0
+    steer = electrics.values['steering_input'] or 0
+    speed = electrics.values.wheelspeed*3.6 or 0 --m/s to km/h
+    local normalSteer = abs(steer) --make value 0 to 1
+    local clampSpeed = clamp(speed, 0, 120) --make speed-steer factor only affect 0-120 km/h
+    
+    --steering contribution with countersteer control
+    local yaw = obj:getYawAngularVelocity() --left is positive
+    if steer*yaw < 0 then
+      xLockCoef = 0
+    else
+      xLockCoef = clamp(lockRange*normalSteer*steerRatio, 0, 1) --calculate steering contribution
+    end
 
-  --set different condition flags
-  local throttleFlag = 0
-  local brakeFlag = 0
-  local lbFlag = 0
-  local coastFlag = 0
-  
-  --select different map flags
-  if throttle > 0 and brake == 0 then --throttle
-    throttleFlag = true
-    brakeFlag = false
-    lbFlag = false
-    coastFlag = false
-  end
-  
-  if throttle == 0 and brake > 0 then --brake
-    throttleFlag = false
-    brakeFlag = true
-    lbFlag = false
-    coastFlag = false
-  end
+    --set different condition flags
+    local throttleFlag = 0
+    local brakeFlag = 0
+    local lbFlag = 0
+    local coastFlag = 0
+    
+    --select different map flags
+    if throttle > 0 and brake == 0 then --throttle
+      throttleFlag = true
+      brakeFlag = false
+      lbFlag = false
+      coastFlag = false
+    end
+    
+    if throttle == 0 and brake > 0 then --brake
+      throttleFlag = false
+      brakeFlag = true
+      lbFlag = false
+      coastFlag = false
+    end
 
-  if brake >= lbThreshold and throttle > 0 then --l.foot brake
-    throttleFlag = false
-    brakeFlag = false
-    lbFlag = true
-    coastFlag = false
-  end
+    if brake >= lbThreshold and throttle > 0 then --l.foot brake
+      throttleFlag = false
+      brakeFlag = false
+      lbFlag = true
+      coastFlag = false
+    end
 
-  if throttle == 0 and brake == 0 then --coast
-    throttleFlag = false
-    brakeFlag = false
-    lbFlag = false
-    coastFlag = true
-  end
+    if throttle == 0 and brake == 0 then --coast
+      throttleFlag = false
+      brakeFlag = false
+      lbFlag = false
+      coastFlag = true
+    end
 
-  --calculate throttle lock map
-  if throttleFlag == true then
-    lockRange = 1 - minLockCoef
-    local contributionThrottle = clamp(lockRange*throttle*(1/throttleRatio) + minLockCoef, minLockCoef, 1)
-    yLockCoef = clamp(contributionThrottle, 0, 1)
-  end
-  
-  --calculate brake lock map
-  if brakeFlag == true then
-    lockRange = 1 - minLockCoef
-    local contributionBrake = clamp(lockRange*brake*(1/brakeRatio) + minLockCoef, minLockCoef, 1)
-    yLockCoef = clamp(contributionBrake, 0, 1)
-  end
+    --calculate throttle lock map
+    if throttleFlag == true then
+      lockRange = 1 - minLockCoef
+      local contributionThrottle = clamp(lockRange*throttle*(1/throttleRatio) + minLockCoef, minLockCoef, 1)
+      yLockCoef = clamp(contributionThrottle, 0, 1)
+    end
+    
+    --calculate brake lock map
+    if brakeFlag == true then
+      lockRange = 1 - minLockCoef
+      local contributionBrake = clamp(lockRange*brake*(1/brakeRatio) + minLockCoef, minLockCoef, 1)
+      yLockCoef = clamp(contributionBrake, 0, 1)
+    end
 
-  --calculate left foot brake map
-  if lbFlag == true then
-    lockRange = lbLockCoef - minLockCoef
-    local contributionLfBrake = clamp(lockRange*abs(throttle-brake) + minLockCoef, minLockCoef, 1)
-    yLockCoef = clamp(contributionLfBrake, 0, 1)
+    --calculate left foot brake map
+    if lbFlag == true then
+      lockRange = lbLockCoef - minLockCoef
+      local contributionLfBrake = clamp(lockRange*abs(throttle-brake) + minLockCoef, minLockCoef, 1)
+      yLockCoef = clamp(contributionLfBrake, 0, 1)
+    end
+
+    --calculate coast map
+    if coastFlag == true then
+      lockRange = 1 - minLockCoef
+      yLockCoef = minLockCoef
+    end
+    
+    --speed map that only affects steering contribution
+    local speedFactor = clamp(clampSpeed*(-9/1200) + 1, 0.1, 1)
+    --print(speedFactor)
+
+    --sum up X and Y lock factors
+    local newLockCoef = clamp(yLockCoef - xLockCoef*speedFactor, 0, 1)
+    
+    if input.parkingbrake > 0.5 then 
+      transfercase.lsdLockCoef = 0
+      transfercase.lsdRevLockCoef = 0
+      transfercase.diffTorqueSplitA = rearBias
+      transfercase.diffTorqueSplitB = 0
+      transfercase.lsdPreload = 0
+    else
+      transfercase.lsdLockCoef = newLockCoef * 0.5
+      transfercase.lsdRevLockCoef = transfercase.lsdLockCoef 
+      transfercase.diffTorqueSplitA = 1- rearBias
+      transfercase.diffTorqueSplitB = rearBias
+      transfercase.lsdPreload = 10
+    end
+  elseif transferType == "Locked" then
+    if input.parkingbrake > 0.5 then 
+      transfercase.diffTorqueSplitA
+      transfercase.diffTorqueSplitA = 0.5
+      transfercase.diffTorqueSplitB = 0
+    else
+      transfercase.diffTorqueSplitA = 0.5
+      transfercase.diffTorqueSplitB = 0.5
+    end
   end
-
-  --calculate coast map
-  if coastFlag == true then
-    lockRange = 1 - minLockCoef
-    yLockCoef = minLockCoef
-  end
-  
-  --speed map that only affects steering contribution
-  local speedFactor = clamp(clampSpeed*(-9/1200) + 1, 0.1, 1)
-  --print(speedFactor)
-
-  --sum up X and Y lock factors
-  local newLockCoef = clamp(yLockCoef - xLockCoef*speedFactor, 0, 1)
-  
-  --handbrake
-  if input.parkingbrake > 0.5 then 
-    newLockCoef = 0
-  end
-
-  --apply values to diff
-  transfercase.lsdLockCoef = newLockCoef * 0.5
-  transfercase.lsdRevLockCoef = transfercase.lsdLockCoef
-  transfercase.diffTorqueSplitA = 1- rearBias
-  transfercase.diffTorqueSplitB = rearBias
-
-  --print(transfercase.lsdLockCoef)
+  print(transfercase.diffTorqueSplitA)
 end
 
 local function init(jbeamData)
   transfercase = powertrain.getDevice(jbeamData.transfercaseName)
+  transferType = jbeamData.type or 0
   
   --get tuning data
-  if transfercase then 
+  if transfercase and transferType == "LSD" then 
     lockMap = tableFromHeaderTable(jbeamData.lockMap or {})
     minLockCoef = lockMap[1].minLock
     steerRatio = lockMap[1].steerRatio
@@ -158,7 +172,7 @@ local function init(jbeamData)
     rearBias = lockMap[1].rearBias
   end
   
-  --printTable(lockMap)
+  printTable(lockMap)
   M.updateWheelsIntermediate = updateWheelsIntermediate
   M.updateGFX = updateGFX
 end
