33ea63f01359806763b1c7b2dc5c19df1d210339
Prog Un1corn <50629932+ProgUn1corn@users.noreply.github.com>
2026-05-05 04:22:27 +0900

    DSV range

--- Lua/LRS2.lua
+++ Lua/LRS2.lua
@@ -75,7 +75,7 @@ local function update(dt)
       --print(loadLenSmooth)
       --print(loadVelSmooth)
       if loadVelSmooth <= -0.46 and loadLenSmooth <= damper.DSV.DSVp then
-        DSVMulti = 1 + (0.2 * damper.DSV.DSVf)
+        DSVMulti = 1 + (0.133 * damper.DSV.DSVf)
         --print(DSVMulti)
       elseif loadVelSmooth >= 0.28 and loadLenSmooth >= damper.DSV.DSVp2 then
         DSVMulti = 1 - (0.167 * damper.DSV.DSVf)
